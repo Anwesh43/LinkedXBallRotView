bf4c9bbecfde48f83aef413636d62b8d9a7ce5cd
Anwesh43 <anweshthecool0@gmail.com>
2019-05-23 10:15:10 +0530

    adding XBRNode class

--- xballrotview/src/main/java/com/anwesh/uiprojects/xballrotview/XBallRotView.kt
+++ xballrotview/src/main/java/com/anwesh/uiprojects/xballrotview/XBallRotView.kt
@@ -128,8 +128,52 @@ class XBallRotView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class XBRNode(var i : Int, val state : State = State()) {
+
+        private var next : XBRNode? = null
+        private var prev : XBRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = XBRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawXBRNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : XBRNode {
+            var curr : XBRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
         }
     }
 }
