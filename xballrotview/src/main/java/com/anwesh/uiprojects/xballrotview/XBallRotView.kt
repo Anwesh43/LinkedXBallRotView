f8a03b7e1d990fd4f1874d79b5febb86a0c4024b
Anwesh43 <anweshthecool0@gmail.com>
2019-05-23 09:32:19 +0530

    adding libraries and constants

--- xballrotview/src/main/java/com/anwesh/uiprojects/xballrotview/XBallRotView.kt
+++ xballrotview/src/main/java/com/anwesh/uiprojects/xballrotview/XBallRotView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.xballrotview
 /**
  * Created by anweshmishra on 23/05/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 2
+val balls : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("283593")
+val backColor : Int = Color.parseColor("#BDBDBD")
