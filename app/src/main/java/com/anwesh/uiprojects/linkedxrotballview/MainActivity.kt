df3ae33e1c7c0021b7bec7106f68c51e4bb29287
Anwesh43 <anweshthecool0@gmail.com>
2019-05-23 10:22:48 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedxrotballview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedxrotballview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedxrotballview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.xballrotview.XBallRotView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        XBallRotView.create(this)
     }
 }
