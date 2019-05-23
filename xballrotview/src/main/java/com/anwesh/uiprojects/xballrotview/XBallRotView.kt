bf4b0cadb1419accddc453020d1709f9c99f01cb
Anwesh43 <anweshthecool0@gmail.com>
2019-05-23 10:20:09 +0530

    using renderer in view

--- xballrotview/src/main/java/com/anwesh/uiprojects/xballrotview/XBallRotView.kt
+++ xballrotview/src/main/java/com/anwesh/uiprojects/xballrotview/XBallRotView.kt
@@ -71,15 +71,16 @@ fun Canvas.drawXBRNode(i : Int, scale : Float, paint : Paint) {
 class XBallRotView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val renderer : Renderer = Renderer(this)
 
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
